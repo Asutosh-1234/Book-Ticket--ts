35ae5144e055db19eb37352aabea32b0b331a67e
Asutosh-1234 <pandaashutosh220@gmail.com>
2026-04-14 13:45:49 +0530

    added the helth check route and controller

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import authRouter from "./modules/auth/auth.route.js";
+import healthCheckRouter from "./modules/helthCheck/helthCheck.route.ts";
+import bookingRouter from "./modules/ticketbooking/ticketbooking.route.ts";
+
+const app = express();
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/v1/auth', authRouter);
+app.use("/healthCheck", healthCheckRouter)
+app.use("/api/v1/booking", bookingRouter)
+
+export default app;
