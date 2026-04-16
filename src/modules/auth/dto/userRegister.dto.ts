8d8b62ac5854beaa81ad91ce866cd9ea15e5c5f8
Asutosh-1234 <pandaashutosh220@gmail.com>
2026-04-17 00:27:53 +0530

    changed the dto of user register

--- src/modules/auth/dto/userRegister.dto.ts
+++ src/modules/auth/dto/userRegister.dto.ts
@@ -4,7 +4,6 @@ const userRegisterDto = z.object({
     name: z.string().min(1, "Name is required").max(255, "Name is too long").trim(),
     email: z.email("Invalid email"),
     password: z.string().min(1, "Password is required").max(255, "Password is too long"),
-    confirmPassword: z.string().min(1, "Confirm Password is required").max(255, "Confirm Password is too long"),
 })
 
 export type UserRegisterDto = z.infer<typeof userRegisterDto>;
