f917927530fee314d1838c615b5d1b8538a8fea9
Asutosh-1234 <pandaashutosh220@gmail.com>
2026-09-12 01:01:52 +0530

    feat: update booking logic to include user validation and enhance error handling
    - Added user validation to check if the user exists before booking a ticket.
    - Improved error handling for seat booking status.
    - Refactored transaction logic for better readability and maintainability.
    
    chore: add docker-compose file for PostgreSQL service
    
    - Created a new docker-compose.yml file to set up a PostgreSQL database service.
    - Configured environment variables for database user, password, and name.
    
    chore: update dependencies in bun.lock
    
    - Added new dependencies including bcrypt, cors, and jsonwebtoken.
    - Updated existing dependencies to their latest versions.

--- src/modules/ticketbooking/ticketbooking.controller.ts
+++ src/modules/ticketbooking/ticketbooking.controller.ts
@@ -10,26 +10,38 @@ import type { bookingDto } from "./dto/booking.dto.ts";
 const booking = async (req: Request, res: Response) => {
     try {
         const { seatNumber, userId, email } = req.body as bookingDto;
-        console.log(seatNumber, userId, email);
 
         if (!seatNumber || !userId || !email) {
             return ApiError.badRequestError(res, "All fields are required");
         }
 
-        const user = await db.select({ id: users.id }).from(users).where(eq(users.email, email));
-        if (!user[0]) {
-            return ApiError.badRequestError(res, "User not found");
-        }
-
         const ticket = await db.transaction(async (tx) => {
-            const checkIsBooked = await tx.select().from(seats).where(eq(seats.seatNumber, seatNumber));
+            const user = await db.select({ id: users.id }).from(users).where(eq(users.email, email));
+            
+            if (!user[0]) {
+                return ApiError.badRequestError(res, "User not found");
+            }
+
+
+            const checkIsBooked = await tx
+            .select()
+            .from(seats)
+            .where(eq(seats.seatNumber, seatNumber));
+
             if (checkIsBooked[0]?.isBooked) {
                 return ApiError.badRequestError(res, "Seat is already booked");
             }
 
-            const bookSeat = await tx.update(seats).set({
+            const bookSeat = await tx.
+            update(seats)
+            .set({
                 isBooked: true,
-            }).where(eq(seats.seatNumber, seatNumber)).returning({ id: seats.id, seatNumber: seats.seatNumber });
+            })
+            .where(eq(seats.seatNumber, seatNumber))
+            .returning({ 
+                id: seats.id, 
+                seatNumber: seats.seatNumber 
+            });
 
             const ticket = await tx.insert(bookings).values({
                 userId: user[0]!.id,
